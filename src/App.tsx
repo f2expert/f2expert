66bb31b775a45594e7268ddd8dc4d639f35d767c
ram.chandar@soprasteria.com <ram.chandar@soprasteria.com>
2025-01-15 12:13:59 +0530

    lint setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css'
+
 function App() {
+
   return (
-    <>
+    <div>
       Hello
-    </>
+    </div>
   )
 }
 
 export default App
+ 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-// tailwind.config.js
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
