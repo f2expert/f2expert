fd5b6e2d185eb75c4a6d73089aa88ad97bbd9399
ram.chandar@soprasteria.com <ram.chandar@soprasteria.com>
2025-01-17 20:56:20 +0530

    resolved build issue

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from 'react'
 
-export default function About() {
+export default function Contact() {
     return (
         <div>
-            About US
+            Contact US
         </div>
     )
 }
