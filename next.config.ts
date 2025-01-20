86867cfb24996fb01c77c4c8324d59ab17982338
ram.chandar@soprasteria.com <ram.chandar@soprasteria.com>
2025-01-20 15:08:24 +0530

    updated f2expert yml

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  // For deployments under a subpath
+  basePath: "/f2expert",
+  assetPrefix: "/f2expert",
 };
 
 export default nextConfig;
