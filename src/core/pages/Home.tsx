f8b365696110a0968a9620720c2213195951ac01
ram.chandar@soprasteria.com <ram.chandar@soprasteria.com>
2025-07-13 18:54:05 +0530

    updated home page content

--- src/core/pages/Home.tsx
+++ src/core/pages/Home.tsx
@@ -1,13 +1,12 @@
-import { Link } from "react-router-dom";
 import Carousel from "../molecules/Carousel";
 import CardTitle from "../atoms/CardTitle";
 import Card from "../molecules/Card";
 import { useEffect, useState } from "react";
 import { fetchCourses } from "../../api/courses/courses";
-import { fetchLatestNews } from "../../api/latestNews/latestNews";
 import { CardContent } from "../../components/ui/card";
 import { GrWorkshop, GrSystem, GrServices, GrProjects } from "react-icons/gr";
-
+import { GiTeamIdea } from "react-icons/gi";
+import { FaPencilRuler } from "react-icons/fa";
 //import { useAppDispatch, useAppSelector } from "../store/hooks";
 //import { login } from "../store/slices/auth";
 interface ICourse {
@@ -19,121 +18,132 @@ interface ICourse {
   alt: string;
   title: string;
 }
-
-interface ILatestNews {
-  id: number;
-  name: string;
-  description: string;
-  duration: string;
-  image: string;
-}
 const Home = () => {
   //const dispatch = useAppDispatch();
   //const count = useAppSelector((state) => state.auth.isLoggedIn);
   const [courses, setCourses] = useState<ICourse[]>([]);
-  const [latestNews, setLatestNews] = useState<ILatestNews[]>([]);
   useEffect(() => {
     fetchCourses().then((data) => {
       setCourses(data);
-    });
-    fetchLatestNews().then((data) => {
-      setLatestNews(data);
     });
   }, []);
 
   return (
     <>
-<Carousel />
+      <Carousel />
       <main>
-         <div className="container mx-auto px-10 py-10 bg-[#f3f3f3]">
-        <div className="max-w-5xl  mx-auto min-h-52 text-center">
-          <div className="w-full py-2">Training we’re offering</div>
-          <h1 className="text-5xl w-full font-bold text-gray-800 leading-tight">
-            We Provide our Students <br />
-            Best IT Content
-          </h1>
+        <div className="container mx-auto px-10 py-10 bg-[#f3f3f3]">
+          <div className="max-w-5xl  mx-auto min-h-52 text-center">
+            <div className="w-full py-2">Training we’re offering</div>
+            <h1 className="text-5xl w-full font-bold text-gray-800 leading-tight">
+              We Provide our Students <br />
+              Best IT Content
+            </h1>
           </div>
           <div className="flex justify-between max-w-5xl gap-6 mx-auto min-h-60 text-center">
-              <CardContent className="bg-[#fbfbfb] text-gray-800 p-6  w-full border-t-8 border-yellow-400">
-                <span className="w-24 h-24 bg-gray-100 inline-block rounded-full p-4">
-                  <GrWorkshop className="text-6xl text-blue-900 mb-3 m-auto" />
-                </span>                
-                <h2 className="text-xl font-semibold p-3">UX Design</h2>
-                <p className="text-gray-600">
-                  Master the art of creating intuitive and visually appealing user interfaces.
-                </p>
-              </CardContent>
-              <CardContent className="bg-[#fbfbfb] text-gray-800 p-5 w-full border-t-8 border-yellow-400">
-                <span className="w-24 h-24 bg-gray-100 inline-block rounded-full p-4">
-                  <GrSystem className="text-6xl text-blue-900 mb-3 m-auto" />
-                </span>                
-                <h2 className="text-xl font-semibold p-3">UI Development</h2>
-                <p className="text-gray-600">
-                  Learn to build responsive and dynamic web applications with modern technologies.
-                </p>
-              </CardContent>
-              <CardContent className="bg-[#fbfbfb] text-gray-800 p-5 w-full border-t-8 border-yellow-400">
-                <span className="w-24 h-24 bg-gray-100 inline-block rounded-full p-4">
-                  <GrServices className="text-6xl text-blue-900 mb-3 m-auto" />
-                </span>                
-                <h2 className="text-xl font-semibold p-3">Backend</h2>
-                <p className="text-gray-600">
-                  Dive into server-side programming and learn how to manage databases and APIs.
-                </p>
-              </CardContent>
-              <CardContent className="bg-[#fbfbfb] text-gray-800 p-5 w-full  border-t-8 border-yellow-400">
-                <span className="w-24 h-24 bg-gray-100 inline-block rounded-full p-4">
-                  <GrProjects className="text-6xl text-blue-900 mb-3 m-auto" />
-                </span>                
-                <h2 className="text-xl font-semibold p-3">Project Management</h2>
-                <p className="text-gray-600">
-                  Gain skills in managing projects effectively, from planning to execution.
-                </p>
-              </CardContent>
+            <CardContent className="bg-[#fbfbfb] text-gray-800 p-6  w-full border-t-8 border-yellow-400">
+              <span className="w-24 h-24 bg-gray-100 inline-block rounded-full p-4">
+                <GrWorkshop className="text-6xl text-blue-900 mb-3 m-auto" />
+              </span>
+              <h2 className="text-xl font-semibold p-3">UX Design</h2>
+              <p className="text-gray-600">
+                Master the art of creating intuitive and visually appealing user
+                interfaces.
+              </p>
+            </CardContent>
+            <CardContent className="bg-[#fbfbfb] text-gray-800 p-5 w-full border-t-8 border-yellow-400">
+              <span className="w-24 h-24 bg-gray-100 inline-block rounded-full p-4">
+                <GrSystem className="text-6xl text-blue-900 mb-3 m-auto" />
+              </span>
+              <h2 className="text-xl font-semibold p-3">UI Development</h2>
+              <p className="text-gray-600">
+                Learn to build responsive and dynamic web applications with
+                modern technologies.
+              </p>
+            </CardContent>
+            <CardContent className="bg-[#fbfbfb] text-gray-800 p-5 w-full border-t-8 border-yellow-400">
+              <span className="w-24 h-24 bg-gray-100 inline-block rounded-full p-4">
+                <GrServices className="text-6xl text-blue-900 mb-3 m-auto" />
+              </span>
+              <h2 className="text-xl font-semibold p-3">Backend</h2>
+              <p className="text-gray-600">
+                Dive into server-side programming and learn how to manage
+                databases and APIs.
+              </p>
+            </CardContent>
+            <CardContent className="bg-[#fbfbfb] text-gray-800 p-5 w-full  border-t-8 border-yellow-400">
+              <span className="w-24 h-24 bg-gray-100 inline-block rounded-full p-4">
+                <GrProjects className="text-6xl text-blue-900 mb-3 m-auto" />
+              </span>
+              <h2 className="text-xl font-semibold p-3">Project Management</h2>
+              <p className="text-gray-600">
+                Gain skills in managing projects effectively, from planning to
+                execution.
+              </p>
+            </CardContent>
           </div>
         </div>
 
-        <div className="container mx-auto px-10 py-10">
+        <div className="container mx-auto px-10 py-10  bg-[#f3f3f3]">
           <div className="flex justify-between max-w-5xl gap-y-10 mx-auto">
-            <div>
-              <h1 className="text-xl font-semibold text-gray-800">
-                What We Offer
-              </h1>
-              <p className="text-gray-600 py-2">
-                F2Expert is a premier UI/UX training institute that specializes
-                in providing top-notch education and hands-on experience in the
-                field of user interface and user experience design.
-                
-Whether you're a beginner looking to start a tech career or a developer aiming to upgrade your skills, our expert-led courses, live projects, and personalized mentorship will guide you every step of the way.
-              </p>
-              <p className="pb-5">
-                <strong>UI Design:</strong>
-                Mastering tools like Figma, Sketch, and Adobe XD to create
-                stunning interfaces. <br />
-                <strong>UX Principles:</strong> Understanding user behavior,
-                research methodologies, and journey mapping.
-                <br />
-                <strong>Prototyping & Wireframing:</strong> Developing
-                interactive prototypes to validate design ideas.
-                <br />
-                <strong>Responsive Design:</strong> Creating adaptable layouts
-                for diverse devices and screen sizes. <br />
-                <strong>Accessibility & Usability:</strong>
-                Ensuring inclusivity and user satisfaction through universal
-                design principles.
-              </p>
-              <Link
-                to="/"
-                className="text-yellow-400 border-[0.1mm] border-yellow-400 px-4 py-3"
-              >
-                Learn more
-              </Link>
+            <div className="image-column w-1/2">
+              <div className="inner-column">
+                <figure className="image-1">
+                  <img src="assets/about-3.jpg" alt="" />
+                </figure>
+                <figure className="image-2">
+                  <img src="assets/about-4.jpg" alt="" />
+                </figure>
+                <div className="experience">
+                  <img src="assets/image-1.jpg" alt="" className="icon" />
+                  <strong>3600+</strong> Satisfied Client
+                </div>
+              </div>
             </div>
-            <img src="assets/about-us.jpg" alt="" width={400} height={333} />
+            <div className="content-column w-1/2">
+              <div className="inner-column ml-16">
+                  <div className="w-full py-2">F2Expert Vision</div>
+                  <h1 className="text-4xl w-full font-bold text-gray-800 pb-5">
+                    We Execute Our ideas <br />
+                    From The Learn to Earn
+                  </h1>
+                <p className="text-gray-500 py-2">
+                  F2Expert is a premier UI/UX training institute that
+                  specializes in providing top-notch education and hands-on
+                  experience in the field of user interface and user experience
+                  design & development. 
+                </p>
+                
+                <div className="flex justify-between gap-3 py-7">
+                  <div className="flex items-center gap-3">
+                    <GiTeamIdea className="text-7xl" />
+                    <div className="p-3">Expert Instructors</div>
+                  </div>
+                  <div className="flex items-center justify-center  gap-3">
+                    <FaPencilRuler className="text-7xl"/>
+                    <div className="p-3">Hands-on Projects</div>
+                  </div>
+                </div>
+                <p className="text-gray-500 py-2">
+                  Our mission is to empower aspiring designers and developers  
+                  Whether you're a beginner looking to start a tech
+                  career or a developer aiming to upgrade your skills, our
+                  expert-led courses, live projects, and personalized mentorship
+                  will guide you every step of the way.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="container mx-auto px-10 py-10">
-          <div className="max-w-5xl mx-auto">
+        <div className="container mx-auto bg-yellow-400">
+          <div className="join">
+            <h3>Click to Join the Advance Workshop</h3>
+            <h1>Training In Advannce UI</h1>
+            <button>Join now</button>
+          </div>          
+        </div>
+        <div className="container mx-auto px-10 py-10">          
+          <div className="max-w-5xl mx-auto mb-10">
             <CardTitle title="Our Courses" label="See All" link="/card" />
           </div>
           <ul className="flex justify-between flex-wrap max-w-5xl gap-y-10 mx-auto">
@@ -155,67 +165,7 @@ Whether you're a beginner looking to start a tech career or a developer aiming t
               </li>
             ))}
           </ul>
-        </div>
-        <div className="container mx-auto bg-yellow-400">
-          <div className="join">
-            <h3>Click to Join the Advance Workshop</h3>
-            <h1>Training In Advannce UI</h1>
-            <button>Join now</button>
-          </div>
-          <div className="story">
-            <Link to="www.google.com">
-              <i className="fa-solid fa-play"></i>
-            </Link>
-            <div className="text">
-              <h1>Success Stories</h1>
-              <p>
-                At F2Expert, we take pride in empowering individuals to
-                transform their aspirations into thriving careers. Here&apos;s
-                an inspiring journey of one of our alumni who mastered the art
-                of web design and development to achieve their professional
-                dreams.
-              </p>
-              <p>
-                <strong>Mentorship Matters:</strong> <br />
-                With one-on-one mentorship from our experienced trainers,
-                students received personalized feedback, guidance, and
-                encouragement to refine their craft. They also benefited from
-                our career support services, including mock interviews and
-                resume-building workshops.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="container mx-auto px-10 py-10 bg-gray-200">
-          <div className="max-w-5xl mx-auto">
-            <CardTitle title="Latest Events" label="See All" link="/card" />
-          </div>
-          <ul className="flex justify-between flex-wrap max-w-5xl gap-y-10 mx-auto">
-            {latestNews.map((news, index) => (
-              <li key={index} className="bg-slate-100 max-w-[325px]">
-                <Card
-                  imgUrl="assets/course-1.jpg"
-                  title="welcome"
-                  description="About Course Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor"
-                  width={325}
-                  height={225}
-                  alt="welcome"
-                  link="/card"
-                  linkClass="text-yellow-400"
-                  linkText="Read More"
-                >
-                  <div className="flex gap-3">
-                    <i className="fa-regular fa-calendar"></i>
-                    <h4 className="space">06 Month</h4>
-                    <i className="fa-regular fa-bookmark"></i>
-                    <h4 className="space"> Photography</h4>
-                  </div>
-                </Card>
-              </li>
-            ))}
-          </ul>
-        </div>
-
+        </div>        
       </main>
     </>
   );
