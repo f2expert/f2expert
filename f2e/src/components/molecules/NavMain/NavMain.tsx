85be73640a78c74a6e1c0074274087af5a22fb94
ram.chandar@soprasteria.com <ram.chandar@soprasteria.com>
2025-11-05 12:58:55 +0530

    fix nav menu url issue

--- f2e/src/components/molecules/NavMain/NavMain.tsx
+++ f2e/src/components/molecules/NavMain/NavMain.tsx
@@ -1,9 +1,7 @@
-import { ChevronRight } from "lucide-react";
 import { Link } from "react-router-dom";
 
 import {
   Collapsible,
-  CollapsibleContent,
   CollapsibleTrigger,
 } from "../Sidebar/collapsible";
 import {
@@ -11,9 +9,6 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
 } from "../Sidebar/sidebar";
 import { iconMap, type IconName } from "../../../store/slices/sidebarDataSlice";
 
@@ -43,32 +38,23 @@ export function NavMain({
           >
             <SidebarMenuItem>
               <CollapsibleTrigger asChild>
-                <SidebarMenuButton tooltip={item.title} className="hover:text-yellow-500">
-                  {item.icon && (() => {
-                    const IconComponent = iconMap[item.icon as keyof typeof iconMap];
-                    return IconComponent ? <IconComponent className="size-3.5" /> : null;
-                  })()}
-                  <span>{item.title}</span>
-                  {item.children && item.children.length > 0 && (
-                    <ChevronRight className="ml-auto size-4 text-gray-500 transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-                  )}
-                </SidebarMenuButton>
+                <Link to={item.path} className="hover:text-yellow-500">
+                  <SidebarMenuButton
+                    tooltip={item.title}
+                    className="hover:text-yellow-500"
+                  >
+                    {item.icon &&
+                      (() => {
+                        const IconComponent =
+                          iconMap[item.icon as keyof typeof iconMap];
+                        return IconComponent ? (
+                          <IconComponent className="size-3.5" />
+                        ) : null;
+                      })()}
+                    {item.title}
+                  </SidebarMenuButton>
+                </Link>
               </CollapsibleTrigger>
-              {item.children && item.children.length > 0 && (
-                <CollapsibleContent>
-                  <SidebarMenuSub>
-                    {item.children.map((subItem) => (
-                      <SidebarMenuSubItem key={subItem.title}>
-                        <SidebarMenuSubButton asChild>
-                          <Link to={subItem.path} className="hover:text-yellow-500">
-                            <span>{subItem.title}</span>
-                          </Link>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    ))}
-                  </SidebarMenuSub>
-                </CollapsibleContent>
-              )}
             </SidebarMenuItem>
           </Collapsible>
         ))}
